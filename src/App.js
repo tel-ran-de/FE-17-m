19dced177321b5a58f3cb00ce5c875883dc019b0
Ignaty Kashnitsky <noir.ik@gmail.com>
2021-06-09 10:43:17 +0200

    Before start writing components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-
+        <section id="form">
+            <form>
+                <input type="text" name="" id=""/>
+                <button>Add</button>
+            </form>
+        </section>
+        <section id="list">
+            <ul>
+                <li>To-Do Item <span>Actions(complete, delete)</span></li>
+            </ul>
+        </section>
     </div>
   );
 }
